dfc8bf38a16e91d017d3314e671fbd7430c05d45
akharikyan <akharikyan@gmail.com>
2022-02-07 14:08:34 +0100

    Initial Commit

---
+++ GH-testing/GH_testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GH_testingApp.swift
+//  GH-testing
+//
+//  Created by Alisa Curtis on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct GH_testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
